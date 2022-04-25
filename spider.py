1be25937f31890513f24806a5daf29983fb638d0
chutzh <chutzh@shanghaitech.edu.cn>
2022-04-25 14:22:40 +0800

    v0.21

--- spider.py
+++ spider.py
@@ -4,7 +4,7 @@ import re
 from tqdm import *
 #Remember to Modify Your Header
 headers = {
-    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36'
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
 }
 # url = 'https://www.pixiv.net/'
 #Remember to Modify Your proxies according to your vpn
